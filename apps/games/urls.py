bc49587b35422e05d0e056f4562270ede1e5b004
PavalRazvan98 <pavalrazvan98@gmail.com>
2024-09-30 16:44:50 +0300

    Interface v1 fix the coments

--- apps/games/urls.py
+++ apps/games/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("homepage/", views.Homepage.as_view(), name="Homepage"),
-    path("game/<int:pk>/", views.DetailViewGame.as_view(), name="GameDetails"),
+    path("homepage/", views.Homepage.as_view(), name="homepage"),
+    path("game/<int:pk>/", views.DetailViewGame.as_view(), name="game-details"),
 ]
